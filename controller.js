75981cbea2df1922be4352082408e76577f9195b
chenemile <chenemile@gmail.com>
2017-01-23 19:10:16 -0600

    Updated to include the Cyrillic transcription

--- controller.js
+++ controller.js
@@ -8,12 +8,15 @@ document.getElementById("button1").onclick = function() {
 	var phonetic_ipa               = graphemes_to_phonemes_ipa(undoubled)
 	var phonetic_krauss1975        = graphemes_to_phonemes_krauss1975(undoubled)
 	var phonetic_nagai2001         = graphemes_to_phonemes_nagai2001(undoubled)
+        var tokenized_cyrillic         = latin_to_cyrillic(tokenized)
+        var corrected_cyrillic         = cyrillic_corrections(tokenized_cyrillic)
 
 	us_ess.innerHTML               = tokens_to_string(tokenized_with_apostrophes)
 	us_ess_undoubled.innerHTML     = tokens_to_string(undoubled)
 	ipa.innerHTML                  = tokens_to_string(phonetic_ipa)
 	krauss1975.innerHTML           = tokens_to_string(phonetic_krauss1975)
 	nagai2001.innerHTML            = tokens_to_string(phonetic_nagai2001)
+        ru_ess.innerHTML               = tokens_to_string(corrected_cyrillic)
 	
 };
 
