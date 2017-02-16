6dcb637050bbe7ddd32991bc20b8548e000d0532
chenemile <chenemile@gmail.com>
2017-02-16 09:51:44 -0600

    Textarea resizes with window size and output matches input style

--- controller.js
+++ controller.js
@@ -14,72 +14,88 @@ document.getElementById("button1").onclick = function() {
         var split_cyrillic_stress_text = []
 
         for (var l = 0; l < split_lines.length; l++) {
-            var split_text = []
-            split_text = split_lines[l].split(" ")
+            if (split_lines[l] == "") {
+                split_latin_text.push("<p>")
+                split_undoubled_text.push("<p>")
+                split_ipa_text.push("<p>")
+                split_krauss_text.push("<p>")
+                split_nagai_text.push("<p>")
+                split_cyrillic_text.push("<p>")
+                split_latin_stress_text.push("<p>")
+                split_ipa_stress_text.push("<p>")
+                split_cyrillic_stress_text.push("<p>")
+            }
 
-            var split_latin           = []
-            var split_undoubled       = []
-            var split_ipa             = []
-            var split_krauss          = []
-            var split_nagai           = []
-            var split_cyrillic        = []
-            var split_latin_stress    = []
-            var split_ipa_stress      = []
-            var split_cyrillic_stress = []
+            else {
+                var split_text = []
+                split_text = split_lines[l].split(" ")
 
-            for (var w = 0; w < split_text.length; w++) {
+                var split_latin           = []
+                var split_undoubled       = []
+                var split_ipa             = []
+                var split_krauss          = []
+                var split_nagai           = []
+                var split_cyrillic        = []
+                var split_latin_stress    = []
+                var split_ipa_stress      = []
+                var split_cyrillic_stress = []
 
-                var lowercased                 = split_text[w].toLowerCase()
-                var tokenized_with_apostrophes = tokenize(lowercased, true)
-                var tokenized                  = tokenize(lowercased, false)
+                for (var w = 0; w < split_text.length; w++) {
 
-                var latin_vowels = new Set(['i', 'a', 'u', 'e'])
-                if (spellcheck(tokenized_with_apostrophes, latin_vowels, original)) {
-                    var undoubled                  = undouble(tokenized)
-                    var phonetic_ipa               = graphemes_to_phonemes_ipa(undoubled)
-                    var adjusted_phonetic_ipa      = ipa_adjust_doubleVowel(phonetic_ipa)
-                    var phonetic_krauss1975        = graphemes_to_phonemes_krauss1975(undoubled)
-                    var phonetic_nagai2001         = graphemes_to_phonemes_nagai2001(undoubled)
-                    var tokenized_cyrillic         = latin_to_cyrillic(undoubled)
-                    var adjusted_cyrillic          = cyrillic_adjustments(tokenized_cyrillic)
+                    var lowercased                 = split_text[w].toLowerCase()
+                    var tokenized_with_apostrophes = tokenize(lowercased, true)
+                    var tokenized                  = tokenize(lowercased, false)
 
-                    var latin_syllabified          = syllabify(tokenized, latin_vowels)
-                    var latin_stressed             = stress(latin_syllabified, latin_vowels)
+                    var latin_vowels = new Set(['i', 'a', 'u', 'e'])
+                    if (spellcheck(tokenized_with_apostrophes, latin_vowels, original)) {
+                        var undoubled                  = undouble(tokenized)
+                        var phonetic_ipa               = graphemes_to_phonemes_ipa(undoubled)
+                        var adjusted_phonetic_ipa      = ipa_adjust_doubleVowel(phonetic_ipa)
+                        var phonetic_krauss1975        = graphemes_to_phonemes_krauss1975(undoubled)
+                        var phonetic_nagai2001         = graphemes_to_phonemes_nagai2001(undoubled)
+                        var tokenized_cyrillic         = latin_to_cyrillic(undoubled)
+                        var adjusted_cyrillic          = cyrillic_adjustments(tokenized_cyrillic)
 
-                    var ipa_vowels = new Set(['\u0069', '\u0251', '\u0075', '\u0259'])
-                    var ipa_syllabified            = syllabify(phonetic_ipa, ipa_vowels)
-                    var ipa_stressed               = stress(ipa_syllabified, ipa_vowels)
-                    var ipa_stress_adjusted        = ipa_adjust_doubleVowel(ipa_stressed)
+                        var latin_syllabified          = syllabify(tokenized, latin_vowels)
+                        var latin_stressed             = stress(latin_syllabified, latin_vowels)
 
-                    var cyrillic_vowels = new Set(['\u0438', '\u0430', '\u0443', '\u044B', '\u04E3', '\u0101', '\u04EF'])
-                    var cyrillic_syllabified       = syllabify(tokenized_cyrillic, cyrillic_vowels)
-                    var cyrillic_stressed          = stress(cyrillic_syllabified, cyrillic_vowels)
-                    var cyrillic_stress_adjusted   = cyrillic_adjustments(cyrillic_stressed)
-                }
-                else {
-                    window.alert("Entry was misspelled. Please try again")
-                }
+                        var ipa_vowels = new Set(['\u0069', '\u0251', '\u0075', '\u0259'])
+                        var ipa_syllabified            = syllabify(phonetic_ipa, ipa_vowels)
+                        var ipa_stressed               = stress(ipa_syllabified, ipa_vowels)
+                        var ipa_stress_adjusted        = ipa_adjust_doubleVowel(ipa_stressed)
 
-                split_latin.push(tokens_to_string(tokenized_with_apostrophes))
-                split_undoubled.push(tokens_to_string(undoubled))
-                split_ipa.push(tokens_to_string(phonetic_ipa))
-                split_krauss.push(tokens_to_string(phonetic_krauss1975))
-                split_nagai.push(tokens_to_string(phonetic_nagai2001))
-                split_cyrillic.push(tokens_to_string(adjusted_cyrillic))
-                split_latin_stress.push(tokens_to_string(latin_stressed).replace(/(\d+)/g, "<sub>$1</sub>"))
-                split_ipa_stress.push(tokens_to_string(ipa_stress_adjusted).replace(/(\d+)/g, "<sub>$1</sub>"))
-                split_cyrillic_stress.push(tokens_to_string(cyrillic_stress_adjusted).replace(/(\d+)/g, "<sub>$1</sub>"))
-            } // End word-'for' Loop
+                        var cyrillic_vowels = new Set(['\u0438', '\u0430', '\u0443', '\u044B', '\u04E3', '\u0101', '\u04EF'])
+                        var cyrillic_syllabified       = syllabify(tokenized_cyrillic, cyrillic_vowels)
+                        var cyrillic_stressed          = stress(cyrillic_syllabified, cyrillic_vowels)
+                        var cyrillic_stress_adjusted   = cyrillic_adjustments(cyrillic_stressed)
+                    }
+                    else {
+                        window.alert("Entry was misspelled. Please try again")
+                    }
 
-            split_latin_text.push(split_latin.join(" "))
-            split_undoubled_text.push(split_undoubled.join(" "))
-            split_ipa_text.push(split_ipa.join(" "))
-            split_krauss_text.push(split_krauss.join(" "))
-            split_nagai_text.push(split_nagai.join(" "))
-            split_cyrillic_text.push(split_cyrillic.join(" "))
-            split_latin_stress_text.push(split_latin_stress.join(" "))
-            split_ipa_stress_text.push(split_ipa_stress.join(" "))
-            split_cyrillic_stress_text.push(split_cyrillic_stress.join(" "))
+                    split_latin.push(tokens_to_string(tokenized_with_apostrophes))
+                    split_undoubled.push(tokens_to_string(undoubled))
+                    split_ipa.push(tokens_to_string(phonetic_ipa))
+                    split_krauss.push(tokens_to_string(phonetic_krauss1975))
+                    split_nagai.push(tokens_to_string(phonetic_nagai2001))
+                    split_cyrillic.push(tokens_to_string(adjusted_cyrillic))
+                    split_latin_stress.push(tokens_to_string(latin_stressed).replace(/(\d+)/g, "<sub>$1</sub>"))
+                    split_ipa_stress.push(tokens_to_string(ipa_stress_adjusted).replace(/(\d+)/g, "<sub>$1</sub>"))
+                    split_cyrillic_stress.push(tokens_to_string(cyrillic_stress_adjusted).replace(/(\d+)/g, "<sub>$1</sub>"))
+
+                } // End word-'for' Loop
+
+                split_latin_text.push(split_latin.join(" "))
+                split_undoubled_text.push(split_undoubled.join(" "))
+                split_ipa_text.push(split_ipa.join(" "))
+                split_krauss_text.push(split_krauss.join(" "))
+                split_nagai_text.push(split_nagai.join(" "))
+                split_cyrillic_text.push(split_cyrillic.join(" "))
+                split_latin_stress_text.push(split_latin_stress.join(" "))
+                split_ipa_stress_text.push(split_ipa_stress.join(" "))
+                split_cyrillic_stress_text.push(split_cyrillic_stress.join(" "))
+            } // End 'else' block
+
         } // End line-'for' Loop
 
         us_ess.innerHTML               = split_latin_text.join("<br>")
